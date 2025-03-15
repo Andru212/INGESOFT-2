223a49ea2e7b4ce3b5ce8f8322b722270b9fe1d5
Andru212 <afsierras@poligran.edu.co>
2025-03-14 20:41:38 -0500

    perros

--- Parcial1/aecardenasfl/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module co.edu.poli {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens co.edu.poli.controlador to javafx.fxml;
-    opens co.edu.poli.vista to javafx.fxml;
-    exports co.edu.poli.controlador;
-    exports co.edu.poli.vista;
-}
