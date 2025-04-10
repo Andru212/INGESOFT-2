873edab40d83346795117e6880721b49fbbc6ec0
Andru212 <afsierras@poligran.edu.co>
2025-04-09 19:40:12 -0500

    PATTERN PROXY

--- Ejemplo/src/main/java/co/edu/poli/ejemplo/controlador/MainController.java
+++ Ejemplo/src/main/java/co/edu/poli/ejemplo/controlador/MainController.java
@@ -1,12 +1,23 @@
 package co.edu.poli.ejemplo.controlador;
 
 import co.edu.poli.ejemplo.modelo.GestorCliente;
+import co.edu.poli.ejemplo.modelo.IProducto;
+import co.edu.poli.ejemplo.modelo.ProductoProxy;
+import co.edu.poli.ejemplo.modelo.ProductoReal;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class MainController {
+
+    @FXML private TextField txtNombre;
+    @FXML private TextField txtPrecio;
+    @FXML private TextField txtNivelUsuario;
+    @FXML private Button btnVerDetalles;
+    @FXML private TextArea txtResultado1;
+
 
     @FXML
     private Button btnActualizarCliente;
@@ -28,8 +39,29 @@ public class MainController {
         txtResultado.setText(" Aplicación iniciada. Usa los botones para interactuar.");
     }
 
+@FXML
+    void VerDetallesProxy() {
+        try {
+            String nombre = txtNombre.getText();
+            float precio = Float.parseFloat(txtPrecio.getText());
+            int nivel = Integer.parseInt(txtNivelUsuario.getText());
 
+            // Crear el producto real
+            ProductoReal productoReal = new ProductoReal(nombre, precio);
 
+            // Crear el proxy con el nivel de usuario
+            IProducto proxy = new ProductoProxy(productoReal, nivel);
+
+            // Mostrar resultado en el Label
+            String resultado = proxy.verDetalles();
+            txtResultado1.setText(resultado);
+
+        } catch (NumberFormatException e) {
+            txtResultado1.setText("⚠️ Error: Precio o nivel no válido.");
+        } catch (Exception e) {
+            txtResultado1.setText("❌ Ocurrió un error inesperado.");
+        }
+    }
 
     @FXML
 void actualizarCliente(ActionEvent event) {
